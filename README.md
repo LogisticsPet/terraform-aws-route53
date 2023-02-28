64351a631af38518ca73fd18f441b85a133da8ff
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-02-28 19:19:39 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Introduction
+Terraform module to provision route53 hosted zone 
+
+<!-- BEGIN_TF_DOCS -->
+<!-- END_TF_DOCS -->
---
+++ main.tf
@@ -0,0 +1,13 @@
+module "zones" {
+  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  version = "~> 2.0"
+
+  zones = {
+    (var.domain) = {
+      comment = "Logistic domain zone"
+      tags    = {
+        type = "public"
+      }
+    }
+  }
+}
