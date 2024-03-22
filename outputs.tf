88f1c0a7de665a7a77566b7df9147f0d5ba1f670
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-03-22 20:04:37 +0100

    cert-manager
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,11 @@ output "nameservers" {
   value       = module.zones.route53_zone_name_servers[var.domain]
   description = "List of Nameservers for Route 53 zone."
 }
+
+output "zone_arn" {
+  value = module.zones.route53_zone_zone_arn[var.domain]
+}
+
+output "zone_id" {
+  value = module.zones.route53_zone_zone_id[var.domain]
+}
